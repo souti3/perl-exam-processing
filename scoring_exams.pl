d4ae72f954e59c1cc3927053b527f90605dff370
Marcel Soltermann <marcel.soltermann@students.fhnw.ch>
2021-09-08 21:15:09 +0200

    [CHG] added a data structure Write questions as key in a hash and add a reference to an array with the answers as value

--- scoring_exams.pl
+++ scoring_exams.pl
@@ -3,6 +3,9 @@ use strict; # optional as a specific version is provided
 use warnings;
 use diagnostics;
 
+# Simplify the display of data structures...
+use Data::Dumper 'Dumper';
+
 # load the self written perl module
 use Utility::Filechecks;
 
@@ -38,18 +41,57 @@ for my $inputFile (@studentFiles) {
 # open the solution file
 open (my $solutionfh, $solutionFile) or die $!;
 
-# regex to match question lines
-my $matchQuestion = qr{^\s*\d+\.\s*\w+.*}xms;
+
+# hash to store question and answers
+my %questions;
+
+# array to store the answers of a answer block
+my @answers;
 
 # read the solution file line by line
 while (my $nextline = readline($solutionfh)) {
+  # regex to match question lines
+  my $matchQuestion = qr{^\s*\d+\.\s*\w+.*}xms;
+  # regex to match answer lines
+  my $matchAnswer = qr{^\s*\[(?:\s*|X\s*)\]\s*.*$}xms;
+  # regex to match the example answers
+  my $matchExampleAnswers = qr{^\s*\[(?:\s+|X\s*)\]\s*This\his.*answer.*$}xms;
+  # state variable to store the current question
+  state $currentQuestion = "no current question";
+  # state variable to store the last processed line
+  state $lastline = "";
+
   chomp $nextline;
+
+  # Copy the introduction as it is - line by line - to the output file
+  if ($nextline !~ $matchAnswer) {
+    # if lastline was an answer, but nextline is not,
+    # the answer block is finished
+    if ($lastline =~ $matchAnswer && $lastline !~ $matchExampleAnswers) {
+      # add the array with the answers in to the hash
+      $questions{$currentQuestion} = [@answers];
+      # clear the array as the next question will follow
+      @answers = ();
+    }
+  }
+
+  # if nextline is an answer, but not an example answer
+  if ($nextline =~ $matchAnswer && $nextline !~ $matchExampleAnswers) {
+    # add the answer into the answer array.
+    push @answers, $nextline;
+  }
+
+
   # if $nextline is a question
   if ($nextline =~ $matchQuestion) {
     # save the question
     say "question: $nextline";
+    $currentQuestion = $nextline;
   }
+
+  $lastline = $nextline;
 }
 
+say Dumper %questions;
 
 say "Program works!";

258bffa1204499cf760ed7d67d17b5cc1d6d4dcc
Marcel Soltermann <marcel.soltermann@students.fhnw.ch>
2021-09-09 16:11:54 +0200

    [CHG] Moved the file read loop in a subroutine Like this it can be called multiple times for solution files as well as for exam files. The return value is a hash with the questions and answers of the file.

--- scoring_exams.pl
+++ scoring_exams.pl
@@ -3,6 +3,8 @@ use strict; # optional as a specific version is provided
 use warnings;
 use diagnostics;
 
+use experimental 'signatures'; # needed to use named arguments
+
 # Simplify the display of data structures...
 use Data::Dumper 'Dumper';
 
@@ -34,64 +36,90 @@ my @studentFiles = @ARGV;
 # file tests with file test operators
 # Using the module Filechecks.pm
 inputcheck($solutionFile);
-for my $inputFile (@studentFiles) {
-  inputcheck($inputFile);
-}
+
 
 # open the solution file
 open (my $solutionfh, $solutionFile) or die $!;
 
+# hash to store question and answers of the solution file
+my %solutionQandA = writeQandAinHash(fileHandle=>$solutionfh);
 
-# hash to store question and answers
-my %questions;
-
-# array to store the answers of a answer block
-my @answers;
-
-# read the solution file line by line
-while (my $nextline = readline($solutionfh)) {
-  # regex to match question lines
-  my $matchQuestion = qr{^\s*\d+\.\s*\w+.*}xms;
-  # regex to match answer lines
-  my $matchAnswer = qr{^\s*\[(?:\s*|X\s*)\]\s*.*$}xms;
-  # regex to match the example answers
-  my $matchExampleAnswers = qr{^\s*\[(?:\s+|X\s*)\]\s*This\his.*answer.*$}xms;
-  # state variable to store the current question
-  state $currentQuestion = "no current question";
-  # state variable to store the last processed line
-  state $lastline = "";
-
-  chomp $nextline;
-
-  # Copy the introduction as it is - line by line - to the output file
-  if ($nextline !~ $matchAnswer) {
-    # if lastline was an answer, but nextline is not,
-    # the answer block is finished
-    if ($lastline =~ $matchAnswer && $lastline !~ $matchExampleAnswers) {
-      # add the array with the answers in to the hash
-      $questions{$currentQuestion} = [@answers];
-      # clear the array as the next question will follow
-      @answers = ();
-    }
-  }
-
-  # if nextline is an answer, but not an example answer
-  if ($nextline =~ $matchAnswer && $nextline !~ $matchExampleAnswers) {
-    # add the answer into the answer array.
-    push @answers, $nextline;
-  }
-
-
-  # if $nextline is a question
-  if ($nextline =~ $matchQuestion) {
-    # save the question
-    say "question: $nextline";
-    $currentQuestion = $nextline;
-  }
-
-  $lastline = $nextline;
+# iterate through the provided exam files from students
+for my $examFile (@studentFiles) {
+  # check wether the files are valid with file test operators
+  inputcheck($examFile);
+  # open the exam file
+  open (my $examfh, $examFile) or die $!;
+  # store the questions and answers in a hash
+  my %studentsQandA = writeQandAinHash(fileHandle=>$examfh);
+  say "Students Q and A";
+  say Dumper %studentsQandA;
+  say "_________________";
+  # close the exam file
+  close $examfh or die $!;
 }
 
-say Dumper %questions;
+# close the solution file
+close $solutionfh or die $!;
+
+
+
+say Dumper %solutionQandA;
 
 say "Program works!";
+
+sub writeQandAinHash ( %args ) {
+  # hash to store the questions together with the answer array
+  my %questionAnswer;
+  # array to store the answers of a answer block
+  my @answers;
+  # file handle provided as argument
+  my $fileHandle = $args{'fileHandle'};
+
+  # read the solution file line by line
+  while (my $nextline = readline($fileHandle)) {
+    # regex to match question lines
+    my $matchQuestion = qr{^\s*\d+\.\s*\w+.*}xms;
+    # regex to match answer lines
+    my $matchAnswer = qr{^\s*\[(?:\s*|X\s*)\]\s*.*$}xms;
+    # regex to match the example answers
+    my $matchExampleAnswers = qr{^\s*\[(?:\s+|X\s*)\]\s*This\his.*answer.*$}xms;
+    # state variable to store the current question
+    state $currentQuestion = "no current question";
+    # state variable to store the last processed line
+    state $lastline = "";
+
+    chomp $nextline;
+
+    # Copy the introduction as it is - line by line - to the output file
+    if ($nextline !~ $matchAnswer) {
+      # if lastline was an answer, but nextline is not,
+      # the answer block is finished
+      if ($lastline =~ $matchAnswer && $lastline !~ $matchExampleAnswers) {
+        # add the array with the answers in to the hash
+        $questionAnswer{$currentQuestion} = [@answers];
+        # clear the array as the next question will follow
+        @answers = ();
+      }
+    }
+
+    # if nextline is an answer, but not an example answer
+    if ($nextline =~ $matchAnswer && $nextline !~ $matchExampleAnswers) {
+      # add the answer into the answer array.
+      push @answers, $nextline;
+    }
+
+
+    # if $nextline is a question
+    if ($nextline =~ $matchQuestion) {
+      # save the question
+      say "question: $nextline";
+      $currentQuestion = $nextline;
+    }
+
+    $lastline = $nextline;
+  }
+
+  # return the hash with all questions and answers of the file
+  return %questionAnswer;
+}
